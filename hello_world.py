35fe8429001f499c298ff84fe5ce5a38116ebe08
Tommi Salonen <salonen.tommi@gmail.com>
2025-05-15 16:15:22 +0300

    korjaus

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,3 @@
-
-def main():
-    print("Hello World!")
-    print("Hello from feature")
-    print("Hello from release")
-
-if __name__ == "__main__":
-    main()
+print("Hello world!")
+print("Hello from feature")
+print("Hello from release")
